73acd30adfbf0be7e3c45f1b29e485ef46a92c07
Richard McGirr <rmcgirr83@gmail.com>
2020-07-18 14:23:41 -0500

    Update code

--- controller/admin_controller.php
+++ controller/admin_controller.php
@@ -10,12 +10,24 @@
 
 namespace apwa\pmwelcome\controller;
 
+/*
+* ignore
+*/
+use phpbb\config\config;
+use phpbb\config\db_text;
+use phpbb\db\driver\driver_interface;
+use phpbb\language\language;
+use phpbb\log\log;
+use phpbb\request\request;
+use phpbb\template\template;
+use phpbb\user;
+
 class admin_controller
 {
 	/**
 	 * sender data
 	 */
-	private $user_info = array();
+	private $sender_info = array();
 
 	/** @var \phpbb\config\config */
 	protected $config;
@@ -26,6 +38,9 @@ class admin_controller
 	/** @var \phpbb\db\driver\driver_interface */
 	protected $db;
 
+	/** @var \phpbb\log\log */
+	protected $log;
+
 	/** @var \phpbb\request\request */
 	protected $request;
 
@@ -35,11 +50,8 @@ class admin_controller
 	/** @var \phpbb\user */
 	protected $user;
 
-	/** @var \phpbb\log\log */
-	protected $log;
-
 	/** @var string phpBB root path */
-	protected $root_path;
+	protected $phpbb_root_path;
 
 	/** @var string phpEx */
 	protected $php_ext;
@@ -53,48 +65,51 @@ class admin_controller
 	* @param \phpbb\config\config									$config				Config object
 	* @param \phpbb\config\db_text 									$config_text		Config text object
 	* @param \phpbb\db\driver\driver_interface						$db					Database object
+	* @param \phpbb\language\language								$language			Language object
+	* @param \phpbb\log\log											$log				Log object
 	* @param \phpbb\request\request									$request			Request object
 	* @param \phpbb\template\template								$template			Template object
 	* @param \phpbb\user											$user				User object
-	* @param \phpbb\log\log											$log				Log object
-	* @param string													$root_path			phpBB root path
+	* @param string													$phpbb_root_path		phpBB root path
 	* @param string													$php_ext			phpEx
 	* @access public
 	*/
 	public function __construct(
-			\phpbb\config\config $config,
-			\phpbb\config\db_text $config_text,
-			\phpbb\db\driver\driver_interface $db,
-			\phpbb\request\request $request,
-			\phpbb\template\template $template,
-			\phpbb\user $user,
-			\phpbb\log\log $log,
-			$root_path,
+			config $config,
+			db_text $config_text,
+			driver_interface $db,
+			language $language,
+			log $log,
+			request $request,
+			template $template,
+			user $user,
+			$phpbb_root_path,
 			$php_ext)
 	{
 		$this->config = $config;
 		$this->config_text = $config_text;
 		$this->db = $db;
+		$this->language = $language;
+		$this->log = $log;
 		$this->request = $request;
 		$this->template = $template;
 		$this->user = $user;
-		$this->log = $log;
-		$this->root_path = $root_path;
+		$this->phpbb_root_path = $phpbb_root_path;
 		$this->php_ext = $php_ext;
 
 		if (!function_exists('display_custom_bbcodes'))
 		{
-			include($this->root_path . 'includes/functions_display.' . $this->php_ext);
+			include($this->phpbb_root_path . 'includes/functions_display.' . $this->php_ext);
 		}
 		if (!function_exists('validate_data'))
 		{
-			include($this->root_path . 'includes/functions_user.' . $this->php_ext);
+			include($this->phpbb_root_path . 'includes/functions_user.' . $this->php_ext);
 		}
 	}
 
 	public function display_options()
 	{
-		$this->user->add_lang(array('acp/board', 'posting'));
+		$this->language->add_lang('posting');
 
 		// Create a form key for preventing CSRF attacks
 		add_form_key('pmwelcome_settings');
@@ -114,69 +129,64 @@ class admin_controller
 
 		$sender_count = (int) $this->pm_welcome_sender_count();
 
-		$this->user_info = $this->pm_welcome_user_name($this->config['pmwelcome_user']);
-		$user_link = '<a href="' . append_sid("{$this->root_path}memberlist.$this->php_ext", 'mode=viewprofile&amp;u=' . $this->user_info['user_id']) . '" target="_blank">' . $this->user_info['username'] . '</a>';
+		$sender_info = $this->pm_welcome_user_name($this->request->variable('pmwelcome_user', $this->config['pmwelcome_user']));
 
-		$pmwelcome_subject = $this->config['pmwelcome_subject'];
+		if (!isset($sender_info['error']))
+		{
+			$user_link = '<a href="' . append_sid("{$this->phpbb_root_path}memberlist.$this->php_ext", 'mode=viewprofile&amp;u=' . $sender_info['user_id']) . '" target="_blank">' . $sender_info['username'] . '</a>';
+		}
+
+		$pmwelcome_subject = $this->request->variable('pmwelcome_subject', $this->config['pmwelcome_subject'], true);
 		$pmwelcome_edit = generate_text_for_edit($pmwelcome_post_text, $pmwelcome_text_uid, $pmwelcome_text_flags);
+		$pmwelcome_post_text = $this->request->variable('pmwelcome_post_text', $pmwelcome_post_text, true);
 
 		if ($this->request->is_set_post('submit')  || $this->request->is_set_post('preview'))
 		{
-			$this->user_info = $this->pm_welcome_user_name($this->request->variable('pmwelcome_user', 0));
-
-			if ($this->request->is_set_post('preview') && !isset($this->user_info['error']))
-			{
-				$user_link = '<a href="' . append_sid("{$this->root_path}memberlist.$this->php_ext", 'mode=viewprofile&amp;u=' . $this->user_info['user_id']) . '" target="_blank">' . $this->user_info['username'] . '</a>';
-			}
-
 			if (!check_form_key('pmwelcome_settings'))
 			{
-				$error[] = $this->user->lang('FORM_INVALID');
+				$error[] = $this->language->lang('FORM_INVALID');
 			}
 
-			if (isset($this->user_info['error']))
+			if (isset($sender_info['error']))
 			{
-				$error[] = $this->user_info['error'];
+				$error[] = $sender_info['error'];
 			}
 
-			$check_row = array(
-				'pmwelcome_subject' => $this->request->variable('pmwelcome_subject', '', true),
-				'pmwelcome_post_text' => $this->request->variable('pmwelcome_post_text', '', true),
-				'pmwelcome_user'	=>	$this->user_info['user_id']);
+			if (utf8_clean_string($pmwelcome_post_text) === '')
+			{
+				$error[] = $this->language->lang('TOO_SHORT_PMWELCOME_POST_TEXT');
+			}
 
-			$validate_row = array(
-				'pmwelcome_subject' => array('string', false, 5, 255),
-				'pmwelcome_post_text' => array('string', false, 1, 2000),
-				'pmwelcome_user'	=> array('num', false, 2, $sender_count));
-
-			$error = validate_data($check_row, $validate_row);
-
-			// Replace "error" strings with their real, localised form
-			$error = array_map(array($this->user, 'lang'), $error);
+			if (utf8_clean_string($pmwelcome_subject) === '')
+			{
+				$error[] = $this->language->lang('TOO_SHORT_PMWELCOME_SUBJECT');
+			}
 
 			if (empty($error) && $this->request->is_set_post('submit'))
 			{
 				$this->config_text->set_array(array(
-					'pmwelcome_post_text'		=> $check_row['pmwelcome_post_text'],
+					'pmwelcome_post_text'		=> $pmwelcome_post_text,
 					'pmwelcome_text_bitfield'	=> $pmwelcome_text_bitfield,
 					'pmwelcome_text_uid'		=> $pmwelcome_text_uid,
 					'pmwelcome_text_flags'		=> $pmwelcome_text_flags,
 				));
 
-				$this->set_options();
+				$this->config->set('pmwelcome_sender', (string) $sender_info['username']);
+				$this->config->set('pmwelcome_user', (int) $sender_info['user_id']);
+				$this->config->set('pmwelcome_subject', $pmwelcome_subject);
 
 				// and an entry into the log table
 				$this->log->add('admin', $this->user->data['user_id'], $this->user->ip, 'LOG_PMWELCOME_CONFIG_UPDATE');
 
 				meta_refresh(5, $this->u_action);
-				trigger_error($this->user->lang('ACP_PMWELCOME_CONFIG_SAVED') . adm_back_link($this->u_action));
+				trigger_error($this->language->lang('ACP_PMWELCOME_CONFIG_SAVED') . adm_back_link($this->u_action));
 			}
 		}
 
 		$pmwelcome_text_preview = '';
 		if ($this->request->is_set_post('preview'))
 		{
-			$pmwelcome_text_preview = (!empty($this->config['pmwelcome_sender'])) ? str_replace('{SENDER}', $this->config['pmwelcome_sender'], $check_row['pmwelcome_post_text']) : $check_row['pmwelcome_post_text'];
+			$pmwelcome_text_preview = (!isset($sender_info['error'])) ? str_replace('{SENDER}', $sender_info['username'], $pmwelcome_post_text) : $pmwelcome_post_text;
 			generate_text_for_storage(
 				$pmwelcome_text_preview,
 				$pmwelcome_text_uid	,
@@ -187,8 +197,7 @@ class admin_controller
 				!$this->request->variable('disable_smilies', false)
 			);
 			$pmwelcome_text_preview = generate_text_for_display($pmwelcome_text_preview, $pmwelcome_text_uid, $pmwelcome_text_bitfield, $pmwelcome_text_flags);
-			$pmwelcome_edit = generate_text_for_edit($check_row['pmwelcome_post_text'], $pmwelcome_text_uid, $pmwelcome_text_flags);
-			$pmwelcome_subject = $check_row['pmwelcome_subject'];
+			$pmwelcome_edit = generate_text_for_edit($pmwelcome_post_text, $pmwelcome_text_uid, $pmwelcome_text_flags);
 		}
 
 		$this->template->assign_vars(array(
@@ -199,8 +208,8 @@ class admin_controller
 			'SENDER_MAX'				=> $sender_count,
 			'SENDER_LINK'				=> $user_link,
 
-			'PMWELCOME_USER'				=> $this->user_info['user_id'],
-			'PMWELCOME_SUBJECT'				=> $pmwelcome_subject,
+			'PMWELCOME_USER'			=> $sender_info['user_id'],
+			'PMWELCOME_SUBJECT'			=> $pmwelcome_subject,
 
 			'S_BBCODE_ALLOWED'		=> true,
 			'S_SMILIES_ALLOWED'		=> true,
@@ -214,34 +223,27 @@ class admin_controller
 		display_custom_bbcodes();
 	}
 
-	public function set_options()
-	{
-		$this->config->set('pmwelcome_sender', (string) $this->user_info['username']);
-		$this->config->set('pmwelcome_user', (int) $this->user_info['user_id']);
-		$this->config->set('pmwelcome_subject', $this->request->variable('pmwelcome_subject', '', true));
-	}
-
 	/**
 	* pm_welcome_user_name
 	*
-	* @param user_id				sender user id
+	* @param sender_id				sender user id
 	* @return array					array of user info or error if not found
 	* @access private
 	*/
-	private function pm_welcome_user_name($user_id)
+	private function pm_welcome_user_name($sender_id)
 	{
 		$sender = array();
 
 		$sql = 'SELECT user_id, username
 			FROM ' . USERS_TABLE . "
-			WHERE user_id = " . (int) $user_id;
+			WHERE user_id = " . (int) $sender_id;
 		$result = $this->db->sql_query($sql);
 		$sender = $this->db->sql_fetchrow($result);
 		$this->db->sql_freeresult($result);
 
 		if (!$sender['username'])
 		{
-			$sender['error'] = $this->user->lang['NO_USER'];
+			$sender['error'] = $this->language->lang('NO_USER');
 		}
 
 		return $sender;
